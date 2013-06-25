e46a9ec53018d61585fb2bfc62b6df4fa57a2e2e
Rob Heaton <rob@pingpanel.com>
2013-06-25 19:26:04 +0100

    Sorted DB, Application.html.haml, flash, routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def home
-
+    
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 CanbanCodeReview::Application.routes.draw do
   devise_for :users
+
+  root to: "pages#home"
 end
